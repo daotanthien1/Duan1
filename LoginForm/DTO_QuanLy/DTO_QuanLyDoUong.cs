62bfda6bcb906988591137444e2eb668e8337617
vanthanh12102002 <hovanthanh12102002@gmail.com>
2021-11-06 20:24:54 -0700

    fix procbeverage

--- LoginForm/DTO_QuanLy/DTO_QuanLyDoUong.cs
+++ LoginForm/DTO_QuanLy/DTO_QuanLyDoUong.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace DTO_QuanLy
 {
-    class DTO_QuanLyDoUong
+    public class DTO_QuanLyDoUong
     {
+        private string name;
+        private double price;
+        private int id_Type;
+        private int id_Beverage;
+        private string image;
+
+        public DTO_QuanLyDoUong()
+        {
+        }
+        //insert
+        public DTO_QuanLyDoUong(string name, double price, int id_Type, string image)
+        {
+            this.name = name;
+            this.price = price;
+            this.id_Type = id_Type;
+            this.image = image;
+        }
+        //update
+        public DTO_QuanLyDoUong(string name, double price, int id_Type, int id_Beverage, string image)
+        {
+            this.name = name;
+            this.price = price;
+            this.id_Type = id_Type;
+            this.id_Beverage = id_Beverage;
+            this.image = image;
+        }
+        
+        public string Name { get => name; set => name = value; }
+        public double Price { get => price; set => price = value; }
+        public int Id_Type { get => id_Type; set => id_Type = value; }
+        public int Id_Beverage { get => id_Beverage; set => id_Beverage = value; }
+        public string Image { get => image; set => image = value; }
+
+       
     }
 }
