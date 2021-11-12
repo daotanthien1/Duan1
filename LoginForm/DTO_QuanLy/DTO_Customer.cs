1cba89eb95b3c3329119e35686c686478a7c8935
vanthanh12102002 <hovanthanh12102002@gmail.com>
2021-11-12 19:27:20 +0700

    Backup 1

--- LoginForm/DTO_QuanLy/DTO_Customer.cs
+++ LoginForm/DTO_QuanLy/DTO_Customer.cs
@@ -10,6 +10,71 @@ namespace DTO_QuanLy
     {
         private string name;
         private string email;
-
+        private int id;
+        private string gender;
+        private int reward;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+        }
+        public int Reward
+        {
+            get
+            {
+                return reward;
+            }
+            set
+            {
+                reward = value;
+            }
+        }
+        public DTO_Customer(string name, string email, string gender, int reward, int id)
+        {
+            this.name = name;
+            this.email = email;
+            this.gender = gender;
+            this.reward = reward;
+            this.id = id;
+        }
     }
 }
--- LoginForm/LoginForm/Program.cs
+++ LoginForm/LoginForm/Program.cs
@@ -17,7 +17,7 @@ namespace LoginForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmSupplier());
+            Application.Run(new FrmBeverageCP());
         }
     }
 }
