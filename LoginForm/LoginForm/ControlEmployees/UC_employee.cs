092d19ea10f616ba40182e6a03238794213676f2
tungnh230802 <tungnh230802@gmail.com>
2021-11-13 09:40:07 +0700

    update db

--- LoginForm/LoginForm/ControlEmployees/UC_employee.cs
+++ LoginForm/LoginForm/ControlEmployees/UC_employee.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 
 namespace RJCodeAdvance.ControlEmployees
 {
-    public partial class UC_employee : Form
+    public partial class UC_employee : UserControl
     {
         BUS_NhanVien busNV = new BUS_QuanLy.BUS_NhanVien();
         public UC_employee()
